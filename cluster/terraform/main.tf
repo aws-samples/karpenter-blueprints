fe240b630da830b91aff9b5e4ab217816ed84311
jakesky <jakesky@amazon.co.uk>
2025-06-23 19:40:54 +0100

    fix: remove irsa for pods identity

--- cluster/terraform/main.tf
+++ cluster/terraform/main.tf
@@ -73,7 +73,6 @@ module "eks" {
   cluster_addons = {
     aws-ebs-csi-driver = {
       most_recent                 = true
-      service_account_role_arn    = module.aws_ebs_csi_pod_identity.iam_role_arn
     }
     coredns = {
       most_recent = true
